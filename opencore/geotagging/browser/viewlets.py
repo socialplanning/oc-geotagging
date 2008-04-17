68d8ff7724854c2f32068c544a64de80f2af7c98
Paul Winkler <slinkp@gmail.com>
2008-04-17 19:53:05 +0000

    Initial progress toward doing the geo UI via viewlets & viewlet managers.  The form displays, but doesn't save yet.
    git-svn-id: https://svn.socialplanning.org/svn/oc-geotagging/trunk@15916 40f73b84-6177-4a8e-8c61-7eee4b1974ed

---
+++ opencore/geotagging/browser/viewlets.py
@@ -0,0 +1,14 @@
+
+class ProjectViewlet:
+
+    sort_order = 123  # we'll sort this out later. HA HA I FUNNY
+
+    def geo_info(self):
+        XXX
+
+    def errors(self):
+        XXX
+
+    
+
+    
