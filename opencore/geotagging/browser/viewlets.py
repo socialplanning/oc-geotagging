b54722db915f6c8ce169cdf160b6e22899be5261
Paul Winkler <slinkp@gmail.com>
2008-04-21 16:44:15 +0000

    Work in progress de-hardcoding dependence on opencore.geotagging: - Do geo javascript via a viewlet & viewlet manager. - Remove some references to geo* from project/browser/
    git-svn-id: https://svn.socialplanning.org/svn/opencore/plugins/oc-geotagging/trunk@16030 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- opencore/geotagging/browser/viewlets.py
+++ opencore/geotagging/browser/viewlets.py
@@ -1,3 +1,6 @@
+import warnings
+from opencore.configuration.utils import get_config
+from urlparse import urlparse
 
 class ProjectViewlet:
 
@@ -11,4 +14,23 @@ class ProjectViewlet:
 
     
 
+    
+class GeoJSViewlet:
+
+    """provides a <script> tag for pages that need geo-related js
+    """
+
+    sort_order = 100
+    
+    def render(self):
+        url = self.request['ACTUAL_URL']
+        # In python 2.5, this could be written as urlparse(url).hostname
+        hostname = urlparse(url)[1].split(':')[0]
+        # We have a map key for each possible hostname in build.ini.
+        key = get_config('google_maps_keys', hostname)
+        if not key:
+            warnings.warn("need a google maps key for %r" % hostname)
+            return ''
+        url = "http://maps.google.com/maps?file=api&v=2&key=%s" % key
+        return '<script src="%s" type="text/javascript"></script>' % url
     
