f13146a201bbf0050b78560bb9b667749ee4dcf2
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2008-08-28 20:56:36 +0000

    fix migration script (coords were reversed)
    git-svn-id: https://svn.socialplanning.org/svn/opencore/plugins/oc-geotagging/branches/viewlet-refactor@20143 40f73b84-6177-4a8e-8c61-7eee4b1974ed

--- geo_migration.py
+++ geo_migration.py
@@ -1,27 +1,32 @@
 from geopy.geocoders import Google
 from Products.PleiadesGeocoder.interfaces import IGeoItemSimple
 
-def geolocate_content(brains):
-    geocoder = Google(resource='maps')
+geocoder = Google(resource='maps')
     
-    for item in brains:
-        content = item.getObject()
-        location = content.getLocation()
-
-        if not location: continue
-        
-        results = geocoder.geocode(location, exactly_one=False)
-        geolocation = None
-
-        for geolocation in results:
-            name, latlon = geolocation
-            content = IGeoItemSimple(content)
-            content.setGeoInterface('Point', latlon)
-            break
-
 mt = app.openplans.membrane_tool
 people = mt(review_state='public')
-geolocate_content(people)
+n = 0
+for item in people:
+    content = item.getObject()
+    location = content.getLocation()
+
+    if not location: continue
+    
+    results = geocoder.geocode(location, exactly_one=False)
+
+    for geolocation in results:
+        # take the first location we get if there are many results
+        name, latlon = geolocation
+        # doReverse = True
+        x,y = latlon
+        lonlat = y,x
+        geocontent = IGeoItemSimple(content)
+        geocontent.setGeoInterface('Point', lonlat)
+        print '%s -> %s %s' % (content.Title(), name, str(lonlat))
+        n += 1
+        break
+
 
 import transaction
+transaction.get().note('updated locations for %s users' % n)
 transaction.commit()
